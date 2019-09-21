2cbee3512dc8753c51d858ef5853a75b16d7ebf9
H3OX <piankovg@gmail.com>
2019-09-21 23:53:25 +0300

    Theme, icons changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquor_advisor/my_flutter_app_icons.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,6 +26,7 @@ class MyAppState extends State<MyApp> {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.dark
       ),
       home: Scaffold(
         appBar: AppBar(
@@ -40,23 +42,23 @@ class MyAppState extends State<MyApp> {
         },
           items: [
             BottomNavigationBarItem(
-              icon: Icon(Icons.keyboard_arrow_left),
+              icon: Icon(MyFlutterApp.coffee),
               title: Text('Чутка')
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.keyboard_arrow_right),
+              icon: Icon(MyFlutterApp.coffee_cup),
               title: Text('Немного')
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.keyboard_arrow_down),
+              icon: Icon(MyFlutterApp.beer),
               title: Text('Средне')
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.keyboard_arrow_up),
+              icon: Icon(MyFlutterApp.wine),
               title: Text('Сильно')
             ),
             BottomNavigationBarItem(
-              icon:Icon(Icons.keyboard_return),
+              icon:Icon(MyFlutterApp.emo_beer),
               title: Text('Очень сильно')
             )
           ]
