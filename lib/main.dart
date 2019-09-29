c251acc3602d0dac86db1f3b7950d5379e2fe427
H3OX <piankovg@gmail.com>
2019-09-30 00:02:11 +0300

    whitespace removed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'package:flutter/services.dart';
 
-
 void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
     .then((x) {
